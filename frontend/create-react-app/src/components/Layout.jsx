e06d28054f3f97dccd938a84f7b5f7f2c7dc354e
Soni Singh <sonisingh90930@gmail.com>
2025-06-30 12:55:27 +0530

    Fix

--- frontend/create-react-app/src/components/Layout.jsx
+++ frontend/create-react-app/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = ({ children }) => {
   return (
-     <div className="min-h-screen w-full bg-gradient-to-r from-purple-500 via-pink-500 to-indigo-500 animate-gradient-x bg-[length:200%_200%]">
+     <div className="min-h-screen w-full bg-gradient-to-r from-purple-500 via-pink-500 to-indigo-500 animate-gradient-x bg-[length:200%_200%] overflow-hidden">
     
       {children}
     </div>
